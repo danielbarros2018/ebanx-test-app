07646da179e02c64c297cf420df72ea078747a81
Daniel Barros <danielbarros.android@gmail.com>
2024-10-29 17:02:18 -0300

    improvements.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,64 +10,94 @@ class EventController extends Controller
 {
     public function postEvent(Request $request)
     {
-        $id = $request->get('destination');
-        $amount = $request->get('amount');
         $operation = $request->get('type');
+        switch ($operation) {
+            case 'deposit':
+                $result = $this->deposit($request);
+                break;
 
-        if ($operation == 'deposit') {
-            $value = Cache::increment($id, $amount);
+            case 'withdraw':
+                $result = $this->withdraw($request);
+                break;
 
-            $result = [
-                'destination' => [
-                    'id' => $id,
-                    'balance' => $value,
-                ],
-            ];
-        }
-
-        if ($operation == 'withdraw') {
-            $id = $request->get('origin');
-            $value = Cache::get($id);
-
-            if ($value == null) {
-                return response()->json(0, Response::HTTP_NOT_FOUND);
-            }
-            $value = Cache::decrement($id, $amount);
-
-            $result = [
-                'origin' => [
-                    'id' => $id,
-                    'balance' => $value,
-                ],
-            ];
+            case 'transfer':
+                $result = $this->transfer($request);
+                break;
 
         }
 
-        if ($operation == 'transfer') {
-            $id = $request->get('origin');
-            $destination = $request->get('destination');
-
-            $totalValue = Cache::get($id);
-            if ($totalValue == null) {
-                return response()->json(0, Response::HTTP_NOT_FOUND);
-            }
-
-            $originValue = Cache::decrement($id, $amount);
-            $value = Cache::increment($destination, $amount);
-
-            $result = [
-                'origin' => [
-                    'id' => $id,
-                    'balance' => $originValue,
-                ],
-                'destination' => [
-                    'id' => $destination,
-                    'balance' => $value,
-                ],
-            ];
+        if (empty($result)) {
+            return response()->json(0, Response::HTTP_NOT_FOUND);
         }
 
         return response()->json($result, Response::HTTP_CREATED);
 
+    }
+
+    private function deposit(Request $request)
+    {
+        $id = $request->get('destination');
+        $amount = $request->get('amount');
+
+        $value = Cache::increment($id, $amount);
+
+        $result = [
+            'destination' => [
+                'id' => $id,
+                'balance' => $value,
+            ],
+        ];
+
+        return $result;
+    }
+
+    private function withdraw(Request $request): array
+    {
+        $id = $request->get('origin');
+        $amount = $request->get('amount');
+        $value = Cache::get($id);
+
+        if ($value == null) {
+            return [];
+        }
+
+        $value = Cache::decrement($id, $amount);
+
+        $result = [
+            'origin' => [
+                'id' => $id,
+                'balance' => $value,
+            ],
+        ];
+
+        return $result;
+    }
+
+    private function transfer(Request $request): array
+    {
+        $id = $request->get('origin');
+        $destination = $request->get('destination');
+        $amount = $request->get('amount');
+
+        $totalValue = Cache::get($id);
+        if ($totalValue == null) {
+            return [];
+        }
+
+        $originValue = Cache::decrement($id, $amount);
+        $destinationValue = Cache::increment($destination, $amount);
+
+        $result = [
+            'origin' => [
+                'id' => $id,
+                'balance' => $originValue,
+            ],
+            'destination' => [
+                'id' => $destination,
+                'balance' => $destinationValue,
+            ],
+        ];
+
+        return $result;
     }
 }
